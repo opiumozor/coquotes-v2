64ad89b4776ddedec68adca046e2228beed5e85b
Julien Peyruchat <julien.peyruchat@gmail.com>
2017-05-16 14:23:02 +0200

    using session messages

--- coquotes/main.py
+++ coquotes/main.py
@@ -1,6 +1,6 @@
 import json
 from coquotes import app
-from flask import render_template, make_response, request, redirect, url_for
+from flask import render_template, make_response, request, redirect, url_for, flash
 from coquotes.auth import requires_auth
 
 @app.route("/")
@@ -33,19 +33,19 @@ def add_quote():
     """
     name = request.form['name']
     quote = request.form['quote']
-    if len(name) < 2 or len(quote) < 2:
-        return make_response("Oops something went wrong!")
-
-    try:
-        quote_list = open(app.config["QUOTES_FILE"]).read()
-    except IOError:
-        quote_list = '[]'
-    entries = json.loads(quote_list)
-    new_quote = {'name': name, 'quote': quote}
-    entries.insert(0, new_quote)
-    try:
-        open(app.config["QUOTES_FILE"], 'w').write(json.dumps(entries, indent=2, sort_keys=True))
-    except IOError:
-        return make_response("Can't add quote", 500)
-
-    return redirect(url_for('index'))
+    if len(name) >= 2 and len(quote) >= 2:
+        try:
+            quote_list = open(app.config["QUOTES_FILE"]).read()
+        except IOError:
+            quote_list = '[]'
+        entries = json.loads(quote_list)
+        new_quote = {'name': name, 'quote': quote}
+        entries.insert(0, new_quote)
+        try:
+            open(app.config["QUOTES_FILE"], 'w').write(json.dumps(entries, indent=2, sort_keys=True))
+            flash(u'Quote has been successfully added', 'success')
+        except IOError:
+            flash(u'Cannot write into file', 'error')
+    else:
+        flash(u'Name and message must contain at least 2 characters', 'warning')
+    return redirect(url_for('add_quote_form'))
